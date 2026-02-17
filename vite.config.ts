b2e56502841223a7b386e92a0aa00405ca9fe024
misticslippers <eternal.martyr13579@gmail.com>
2026-02-17 08:37:44 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    define: {
+        global: 'window',
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/ws': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true,
+            },
+        },
+    },
+});
